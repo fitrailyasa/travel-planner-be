867eaeb2f2955af45ac962e04f4db94922552d37
“gerry” <gerrydesrian488@gmail.com>
2025-02-01 10:43:06 +0700

    feat/destination validation

--- src/routes/v1/destination.route.js
+++ src/routes/v1/destination.route.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const auth = require('../../middlewares/auth');
-// const validate = require('../../middlewares/validate');
-// const userValidation = require('../../validations/user.validation');
+const validate = require('../../middlewares/validate');
+const destinationValidation = require('../../validations/destination.validation');
 const destinationController = require('../../controllers/destination.controller');
 
 const router = express.Router();
 
-router.route('/').post(destinationController.createDestination).get(destinationController.getAllDestination);
+router
+  .route('/')
+  .post(validate(destinationValidation.createDestination), destinationController.createDestination)
+  .get(destinationController.getAllDestination);
 
 router
   .route('/:destinationId')
-  .get(destinationController.getDestinationById)
-  .patch(destinationController.updateDestination)
-  .delete(destinationController.deleteDestination);
+  .get(validate(destinationValidation.getDeleteDestinationById), destinationController.getDestinationById)
+  .patch(validate(destinationValidation.updateDestination), destinationController.updateDestination)
+  .delete(validate(destinationValidation.getDeleteDestinationById), destinationController.deleteDestination);
 
 module.exports = router;
